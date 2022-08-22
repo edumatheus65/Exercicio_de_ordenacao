3de569de26476352d8873d182b17fbd6d3c89b98
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-22 10:22:56 -0300

    Criado tabela com table header name & idade

--- index.php
+++ index.php
@@ -3,3 +3,26 @@
 include_once("conection.php");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <table border="1" width="400">
+
+        <tr>
+            <th>Nome</th>
+            <th>Idade</th>
+        </tr>
+
+
+    </table>
+    
+</body>
+</html>
